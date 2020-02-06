20ae03615c3da50ef716313b145eb9a62132abe2
liaoyuhao <342129019@qq.com>
2020-02-06 18:26:50 -0330

    Adding 3 files

--- lab1.py
+++ lab1.py
@@ -12,27 +12,29 @@ for k in range(1, 5):
     print(k)
     inputName = 'test' + str(k) + '.jpg'
     outputName = 'a' + str(k) + '.jpg'
-    
-    img = cv2.imread(inputName) # Remember to add the path for the test1.jpg
+
+    img = cv2.imread(inputName)  # Remember to add the path for the test1.jpg
     size = img.shape
-    
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # 100, 500
-    edges = cv2.Canny(gray,50,800) # The parameters are the thresholds for Canny
-    
-    lines = cv2.HoughLines(edges,1, np.pi / 180, 160) # The parameters are accuracies and threshold
+    # The parameters are the thresholds for Canny
+    edges = cv2.Canny(gray, 50, 800)
+
+    # The parameters are accuracies and threshold
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 160)
     num = len(lines)
     for n in range(num):
         rho, theta = lines[n][0]
         a = np.cos(theta)
         b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + size[1]*(-b))
-        y1 = int(y0 + size[0]*(a))
-        x2 = int(x0 - size[1]*(-b))
-        y2 = int(y0 - size[0]*(a))
-    
-        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-    
-    cv2.imwrite(outputName,img)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + size[1] * (-b))
+        y1 = int(y0 + size[0] * (a))
+        x2 = int(x0 - size[1] * (-b))
+        y2 = int(y0 - size[0] * (a))
+
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imwrite(outputName, img)
