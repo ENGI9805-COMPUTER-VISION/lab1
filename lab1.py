fcc1caf78c663a3aa6f8db69fdf0fbffb2fe8e26
liaoyuhao <342129019@qq.com>
2020-02-08 23:53:50 -0330

    retrieving all the intersections

--- lab1.py
+++ lab1.py
@@ -9,9 +9,11 @@ import cv2
 import numpy as np
 import os
 
+from intersection import segment_by_angle_kmeans, segmented_intersections
 
-def drawLines(filename, path=''):
-    img = cv2.imread(os.path.join(path, filename))  # Remember to add the path for the test1.jpg
+
+def drawLine(filename, input_path='', output_path='results'):
+    img = cv2.imread(os.path.join(input_path, filename))  # Remember to add the path for the test1.jpg
     size = img.shape
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -33,5 +35,23 @@ def drawLines(filename, path=''):
 
         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-    cv2.imwrite(os.path.join("results", filename), img)
-    return img
+    cv2.imwrite(os.path.join(output_path, filename), img)
+    return img, lines
+
+
+def drawLines():
+    for i in range(1, 5):
+        input_name = 'test{}.jpg'.format(i)
+        _, lines = drawLine(input_name)
+        return lines
+
+
+def drawIntersections(lines):
+    segmented = segment_by_angle_kmeans(lines)
+    intersections = segmented_intersections(segmented)
+    return intersections
+
+
+if __name__ == "__main__":
+    lines = drawLines()
+    drawIntersections(lines)
